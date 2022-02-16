137408044e67910ac9fe94837d7640b6afc62cae
teja1297 <89035725+teja1297@users.noreply.github.com>
2022-02-16 17:13:16 +0530

    Update SupplyChain.sol

--- SupplyChain/contracts/SupplyChain.sol
+++ SupplyChain/contracts/SupplyChain.sol
@@ -2,19 +2,20 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 contract supplyChain {
-    uint32 public product_id = 0;   // Product ID
-    uint32 public participant_id = 0;   // Participant ID
-    uint32 public owner_id = 0;   // Ownership ID
-    uint public temparatureControl = 5;
+    uint32 public product_id = 1;   // Product ID
+    uint32 public participant_id = 1;   // Participant ID
+    uint32 public owner_id = 1;   // Ownership ID
+    string[] public participantTypeList =["Manufacturer","Distributor","Wholesaler","Pharmacy"];
 
     struct Drug {
         string drugName;
         string drugCode;
         address productOwner;
         uint32 cost;
-        uint32 mfgTimeStamp;
-        uint32 expTimeStamp;
-        uint8  temperature;
+        uint mfgTimeStamp;
+        uint expTimeStamp;
+        uint32  CurrentTemperature;
+        uint32 IdealTemperature;
         uint32 batchID;
         string currentLocation;
         string status;
@@ -28,6 +29,7 @@ contract supplyChain {
         string participantType;
         address participantAddress;
     }
+
     mapping(uint32 => participant) public participants;
 
     struct ownership {
@@ -36,12 +38,18 @@ contract supplyChain {
         uint32 trxTimeStamp;
         address productOwner;
     }
+
     mapping(uint32 => ownership) public ownerships; // ownerships by ownership ID (owner_id)
     mapping(uint32 => uint32[]) public productTrack;  // ownerships by Product ID (product_id) / Movement track for a product
     event TransferOwnership(uint32 productId);
     event Expiry(Drug drug);
     event overTemp(Drug drug);
-    
+
+
+
+    //fallback function
+fallback()external{}
+
     function addParticipant(string memory _name, string memory _pass, address _pAdd, string memory _pType) public returns (uint32){
         uint32 userId = participant_id++;
         participants[userId].userName = _name;
@@ -52,13 +60,14 @@ contract supplyChain {
         return userId;
     }
 
+
     function getParticipant(uint32 _participant_id) public view returns (string memory,address,string memory) {
         return (participants[_participant_id].userName,
                 participants[_participant_id].participantAddress,
                 participants[_participant_id].participantType);
     }
 
-    function addProduct(uint32 _ownerId,
+    function addProduct(uint32 _ownerId,//change to userID
                         string memory _drugName,
                         string memory _drugCode,
                         string memory _currentLocation,
@@ -67,110 +76,70 @@ contract supplyChain {
                         uint32 _manufacturingTimeStamp,
                         uint32 _expiryTimeStamp,
                         uint32 _batchID,
-                        uint8 _temperature
+                        uint32 _CurrentTemperature,
+                        uint32 _Idealtemperature
                         ) public returns (uint32) {
 
-        if(keccak256(abi.encodePacked(participants[_ownerId].participantType)) == keccak256("Manufacturer")) {
+        require(keccak256(abi.encodePacked(participants[_ownerId].participantType)) == keccak256("Manufacturer")) ;
+        
             uint32 productId = product_id++;
 
-            products[productId].drugName = _drugName;
-            products[productId].drugCode = _drugCode;
-            products[productId].currentLocation = _currentLocation;
-            products[productId].cost = _productCost;
-            products[productId].productOwner = participants[_ownerId].participantAddress;
-            products[productId].mfgTimeStamp = _manufacturingTimeStamp;
-            products[productId].expTimeStamp = _expiryTimeStamp;
-            products[productId].status = _status;
-            products[productId].batchID = _batchID;
-            products[productId].temperature = _temperature;
+            Drugs[productId].drugName = _drugName;
+            Drugs[productId].drugCode = _drugCode;
+            Drugs[productId].currentLocation = _currentLocation;
+            Drugs[productId].cost = _productCost;
+            Drugs[productId].productOwner = participants[_ownerId].participantAddress;
+            Drugs[productId].mfgTimeStamp = _manufacturingTimeStamp;
+            Drugs[productId].expTimeStamp = _expiryTimeStamp;
+            Drugs[productId].status = _status;
+            Drugs[productId].batchID = _batchID;
+            Drugs[productId].CurrentTemperature = _CurrentTemperature;
+             Drugs[productId].IdealTemperature = _Idealtemperature;
 
             return productId;
-        }
+        
 
-       return 0;
+       
     }
 
-    modifier onlyOwner(uint32 _productId) {
-         require(msg.sender == products[_productId].productOwner,"");
-         _;
+   
 
-    }
-    
-    modifier isExpired(uint32 _productId) {
-         if(products[_prodId].expTimeStamp < uint32(now))
-         {
-             emit Expiry(products[_prodId]);
-         }
-         _;
-
+    function getProduct(uint32 _productId) public view returns (Drug memory drug){
+        return ( Drugs[_productId]  );
     }
 
-    modifier isUnderTemp (uint32 _productId) {
-         if(products[_prodId].temperature > temparatureControl){
-             emit OverTemp(products[_prodId]);
-         }
-         _;
-
-    }
-
-
-    function getProduct(uint32 _productId) public view returns (string memory,string memory,string memory,uint32,string memory,string memory,uint32,uint32,uint32,uint8){
-        return (products[productId].drugName ,
-            products[productId].drugCode,
-            products[productId].currentLocation ,
-            products[productId].cost ,
-            products[productId].productOwner ,
-            products[productId].mfgTimeStamp ,
-            products[productId].expTimeStamp,
-            products[productId].status ,
-            products[productId].batchID ,
-            products[productId].temperature);
-    }
-
-    function newOwner(uint32 _user1Id,uint32 _user2Id, uint32 _prodId) onlyOwner(_prodId),isExpired(_prodId),isUnderTemp(_prodId) public returns (bool) {
+    function newOwner(uint32 _user1Id,uint32 _user2Id, uint32 _prodId) onlyOwner(_prodId) isExpired(_prodId) isUnderTemp(_prodId) public returns (bool) {
         participant memory p1 = participants[_user1Id];
         participant memory p2 = participants[_user2Id];
         uint32 ownership_id = owner_id++;
-        require();
-        if(keccak256(abi.encodePacked(p1.participantType)) == keccak256("Manufacturer")
-            && keccak256(abi.encodePacked(p2.participantType))==keccak256("Distributor")){
+        uint  participant_id1;
+        uint  participant_id2;
 
-            ownerships[ownership_id].productId = _prodId;
+        for(uint i=0;i<participantTypeList.length;i++){
+            if(keccak256(abi.encodePacked(p1.participantType)) == keccak256(abi.encodePacked(participantTypeList[i])))
+            {
+                    participant_id1 =i;
+            }
+            if(keccak256(abi.encodePacked(p2.participantType)) == keccak256(abi.encodePacked(participantTypeList[i])))
+            {
+                    participant_id2 =i;
+            }
+        }
+
+        if((participant_id2 - participant_id1)==1){
+             ownerships[ownership_id].productId = _prodId;
             ownerships[ownership_id].productOwner = p2.participantAddress;
             ownerships[ownership_id].ownerId = _user2Id;
-            ownerships[ownership_id].trxTimeStamp = uint32(now);
-            products[_prodId].productOwner = p2.participantAddress;
+            ownerships[ownership_id].trxTimeStamp = uint32(block.timestamp);
+            Drugs[_prodId].productOwner = p2.participantAddress;
             productTrack[_prodId].push(ownership_id);
             emit TransferOwnership(_prodId);
             return (true);
         }
-        else if(keccak256(abi.encodePacked(p1.participantType)) == keccak256("Distributor") && keccak256(abi.encodePacked(p2.participantType))==keccak256("Wholesaler")){
-            ownerships[ownership_id].productId = _prodId;
-            ownerships[ownership_id].productOwner = p2.participantAddress;
-            ownerships[ownership_id].ownerId = _user2Id;
-            ownerships[ownership_id].trxTimeStamp = uint32(now);
-            products[_prodId].productOwner = p2.participantAddress;
-            productTrack[_prodId].push(ownership_id);
-            emit TransferOwnership(_prodId);
-
-            return (true);
-        }
-        else if(keccak256(abi.encodePacked(p1.participantType)) == keccak256("Wholesaler") && keccak256(abi.encodePacked(p2.participantType))==keccak256("Pharmacy")){
-            ownerships[ownership_id].productId = _prodId;
-            ownerships[ownership_id].productOwner = p2.participantAddress;
-            ownerships[ownership_id].ownerId = _user2Id;
-            ownerships[ownership_id].trxTimeStamp = uint32(now);
-            products[_prodId].productOwner = p2.participantAddress;
-            productTrack[_prodId].push(ownership_id);
-            emit TransferOwnership(_prodId);
-
-            return (true);
-        }
-
         return (false);
     }
 
-   function getProvenance(uint32 _prodId) external view returns (uint32[] memory) {
+   function getProvenance(uint32 _prodId) external view returns (uint32[] memory){
 
        return productTrack[_prodId];
     }
@@ -196,4 +165,31 @@ contract supplyChain {
 
         return (false);
     }
+
+
+
+ modifier onlyOwner(uint32 _productId) {
+         require(msg.sender == Drugs[_productId].productOwner,"");
+         _;
+}
+    
+
+    modifier isExpired(uint32 _productId) {
+         if(Drugs[_productId].expTimeStamp < block.timestamp)
+         {
+             emit Expiry(Drugs[_productId]);
+         }
+         _;
+
+    }
+
+    modifier isUnderTemp (uint32 _productId) {
+         if(Drugs[_productId].CurrentTemperature > Drugs[_productId].IdealTemperature){
+             emit overTemp(Drugs[_productId]);
+         }
+         _;
+
+    }
+
+
 }
